2c46ebf5f21015ae3665ff7f546021284f9f577a
Clarence Chan <clarencechaan@icloud.com>
2022-09-28 19:48:06 -0400

    added route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Thread from "./components/Thread";
 import Home from "./components/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/r/:subreddit/comments/:threadId/*" element={<Thread />} />
         <Route path="/comments/:threadId" element={<Thread />} />
         <Route path="/*" element={<Home />} />
       </Routes>
