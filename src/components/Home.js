08838b6e50052cac7163fb5fb7204bf6e82f35d4
Clarence Chan <clarencechaan@icloud.com>
2022-10-03 15:26:34 -0400

    about section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import "../styles/Home.css";
 import Sidebar from "./Sidebar";
+import logo from "../images/logo.png";
+import github from "../images/github.png";
 
 function Home() {
   return (
     <div className="Home">
       <Sidebar />
-      <div className="main"></div>
+      <div className="main">
+        <img src={logo} className="logo" alt="" />
+        <div className="tag-line">live threads for reddit</div>
+        <a href="https://github.com/clarencechaan" className="me">
+          by Clarence Chan <img src={github} className="github" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
