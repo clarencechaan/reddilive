c854c6af1bfd132628e010705b3966a79c416dad
Clarence Chan <clarencechaan@icloud.com>
2022-10-05 22:13:24 -0400

    Comment styling when user is logged in

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,11 +1,13 @@
 import "../styles/Comment.css";
 import { getTimeAgo, getSecondsAgo } from "../scripts/timeConversion";
-import { ArrowUp } from "phosphor-react";
+import { ArrowUp, ArrowDown, ChatsCircle } from "phosphor-react";
 import { formatBody, formatFlair } from "../scripts/markdown";
-import { useRef } from "react";
+import { useRef, useContext } from "react";
+import UserContext from "../UserContext";
 
 function Comment({ comment, delay, now }) {
   const repliesRef = useRef(null);
+  const { user, setUser } = useContext(UserContext);
 
   const replies =
     comment.data.replies?.data?.children.filter(
@@ -28,6 +30,25 @@ function Comment({ comment, delay, now }) {
     repliesRef.current.classList.toggle("collapsed");
   }
 
+  const score = user ? (
+    <label className="score">
+      <button>
+        <ArrowUp size={14} weight="bold" />
+      </button>
+      {comment.data.score}
+      {user ? (
+        <button>
+          <ArrowDown size={14} weight="bold" />
+        </button>
+      ) : null}
+    </label>
+  ) : (
+    <label className="score">
+      <ArrowUp size={14} weight="bold" />
+      {comment.data.score}
+    </label>
+  );
+
   return getSecondsAgo(comment.data.created, { now }) <= delay ? null : (
     <div className="Comment">
       <div className="bubble">
@@ -42,12 +63,13 @@ function Comment({ comment, delay, now }) {
           ) : (
             <span className="author deleted">[deleted]</span>
           )}
-
-          <label className="score">
-            <ArrowUp size={14} weight="bold" />
-            {comment.data.score}
-          </label>
+          {score}
           {flair}
+          {user ? (
+            <button className="reply-btn">
+              <ChatsCircle size={14} weight="fill" />
+            </button>
+          ) : null}
           <a
             href={`https://reddit.com${comment.data.permalink}`}
             className="timestamp"
