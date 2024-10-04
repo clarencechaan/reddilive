7c62beef3425a45be89a9577d384fc35d4de6720
Clarence Chan <clarencechaan@icloud.com>
2024-10-04 14:32:52 -0400

    Convert to arrow function

--- src/components/Throbber.tsx
+++ src/components/Throbber.tsx
@@ -4,21 +4,19 @@ import "../styles/Throbber.css";
 /**
  * Component for a throbber animation consisting of eight blocks.
  */
-function Throbber() {
-  return (
-    <div className="Throbber">
-      <div className="block">
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item"></div>
-      </div>
+const Throbber = () => (
+  <div className="Throbber">
+    <div className="block">
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
+      <div className="item"></div>
     </div>
-  );
-}
+  </div>
+);
 
 export default Throbber;
