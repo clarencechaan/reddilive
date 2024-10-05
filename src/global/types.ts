0a613d85ca29196c96a54c187b3a607a4510b975
Clarence Chan <clarencechaan@icloud.com>
2024-10-05 15:47:19 -0400

    Add global directory

---
+++ src/global/types.ts
@@ -0,0 +1,24 @@
+export type RedditComment = {
+  kind: "t1" | "more";
+  data: {
+    author: string;
+    author_flair_richtext: object[];
+    author_flair_text: string | null;
+    body: string;
+    created: number;
+    depth: number;
+    id: string;
+    likes: boolean | null;
+    media_metadata: object;
+    permalink: string;
+    replies: { data: { children: RedditComment[] } };
+    score: number;
+    score_hidden: boolean;
+  };
+};
+
+export type SetStateFunction = () => void;
+
+export type Thread = {
+  comments: RedditComment[];
+};
