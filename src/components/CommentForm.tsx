aef99a168deec5bd1478da203f85b0e6dc421a50
Clarence Chan <clarencechaan@icloud.com>
2024-10-08 14:34:48 -0400

    Convert CommentForm to TypeScript

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,54 +1,57 @@
 import { cloneDeep } from "lodash";
-import React, { useContext, useRef } from "react";
+import React, { useContext } from "react";
 import UserContext from "../context/UserContext";
 import "../styles/CommentForm.css";
 import { submitComment } from "../utils/redditAPI";
 import { getSecondsAgo } from "../utils/timeConversion";
+import { RedditComment, RedditThread } from "../global/types";
+
+interface CommentFormProps {
+  parentFullname: string;
+  setThread: ((cb: (prev: RedditThread) => RedditThread) => void) | null;
+  setComment: (cb: (prev: RedditComment) => RedditComment) => void;
+  parentAuthor: string;
+  delay: number;
+  now: number;
+}
 
 /**
  * Component for submitting comments to a Reddit thread.
- *
- * @param {string} parentFullname - The unique identifier of the parent thread.
- * @param {function} setThread - A function to set the state of the parent
- *                               thread if the comment is successfully
- *                               submitted.
- * @param {function} setComment - A function to set the state of the current
- *                                comment if the comment is successfully
- *                                submitted.
- * @param {string} parentAuthor - The username of the author of the parent thread.
- * @param {number} delay - The delay before comments are visible, in seconds.
- * @param {number} now - The current time.
  */
-function CommentForm({
+const CommentForm = ({
   parentFullname,
   setThread,
   setComment,
   parentAuthor,
   delay,
   now,
-}) {
+}: CommentFormProps) => {
   const { user } = useContext(UserContext);
-  const commentFormRef = useRef(null);
 
   /**
    * Attempts to submit the comment to the Reddit thread and update the state of
    * the parent thread or the current comment if successful.
-   *
-   * @param {object} e - The event object.
    */
-  async function handleCommentFormSubmit(e) {
+  const handleCommentFormSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
     e.preventDefault();
+
     const parent = parentFullname;
-    const text = e.target[0].value;
+    const form = e.target as HTMLFormElement;
+    const textArea = form[0] as HTMLTextAreaElement;
+    const text = textArea.value;
+
     if (!text) return;
-    e.target[0].disabled = true;
+    textArea.disabled = true;
+
     try {
       const comment = await submitComment(parent, text);
       if (comment && setThread)
         setThread((prev) => {
-          let result = cloneDeep(prev);
-          let idx = result.comments.findLastIndex(
-            (c) =>
+          const result = cloneDeep(prev);
+          const idx = result.comments.findLastIndex(
+            (c: RedditComment) =>
               getSecondsAgo(c.data.created, { now }) >= delay ||
               user === c.data.author
           );
@@ -61,7 +64,7 @@ function CommentForm({
         });
       else if (comment && setComment)
         setComment((prev) => {
-          let result = cloneDeep(prev);
+          const result = cloneDeep(prev);
           if (!result.data.replies)
             result.data.replies = { data: { children: [comment] } };
           else
@@ -71,44 +74,40 @@ function CommentForm({
             ];
           return result;
         });
-      e.target[0].disabled = false;
-      e.target[0].style.minHeight = "12px";
-      e.target.reset();
+      textArea.disabled = false;
+      textArea.style.minHeight = "12px";
+      form.reset();
     } catch (error) {
       console.log("error", error);
     }
-  }
+  };
 
   /**
    * Resizes the text input based on the content height.
-   *
-   * @param {object} textInput - The text input element.
    */
-  function resizeTextInput(textInput) {
-    textInput.style.minHeight = "0px";
-    textInput.style.minHeight =
-      Math.min(textInput.scrollHeight + 2, 129) + "px";
-  }
+  const resizeTextInput = (textArea: HTMLTextAreaElement) => {
+    textArea.style.minHeight = "0px";
+    textArea.style.minHeight = Math.min(textArea.scrollHeight + 2, 129) + "px";
+  };
 
   /**
    * Resizes the input when the user types.
-   *
-   * @param {object} e - The event object.
    */
-  function handleTextInputChanged(e) {
-    resizeTextInput(e.target);
-  }
+  const handleTextInputChanged = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    resizeTextInput(e.target as HTMLTextAreaElement);
+  };
 
   /**
    * Submits comment to reddit when Enter key is pressed.
-   *
-   * @param {object} e - The event object.
    */
-  function onEnterPress(e) {
-    if (e.keyCode === 13 && !e.shiftKey) {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
-      e.target.blur();
-      const form = commentFormRef.current;
+      const textArea = e.target as HTMLTextAreaElement;
+      textArea.blur();
+      const form = textArea.parentNode as HTMLFormElement;
       if (form) {
         if (typeof form.requestSubmit === "function") {
           form.requestSubmit();
@@ -117,29 +116,22 @@ function CommentForm({
         }
       }
     }
-  }
+  };
 
   return (
-    <form
-      action=""
-      className="CommentForm"
-      onSubmit={handleCommentFormSubmit}
-      ref={commentFormRef}
-    >
+    <form action="" className="CommentForm" onSubmit={handleCommentFormSubmit}>
       {user ? (
         <textarea
-          type="text"
           placeholder={
             parentAuthor ? `Reply to ${parentAuthor}...` : "Write a comment..."
           }
-          onKeyDown={onEnterPress}
+          onKeyDown={handleKeyDown}
           onChange={handleTextInputChanged}
           maxLength={10000}
           enterKeyHint="send"
         />
       ) : (
         <textarea
-          type="text"
           placeholder={
             parentAuthor
               ? `Log in to reply to ${parentAuthor}... `
@@ -150,6 +142,6 @@ function CommentForm({
       )}
     </form>
   );
-}
+};
 
 export default CommentForm;
