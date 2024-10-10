c0dbd55b3ed5edf682acb0ccf93688bac15107c8
Clarence Chan <clarencechaan@icloud.com>
2024-10-10 14:09:55 -0400

    Convert Auth to TypeScript

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -6,47 +6,35 @@ import { fetchMe } from "../utils/redditAPI";
 import Home from "./Home";
 
 /**
- * Component that handles user authentication through OAuth 2.0.
- * The authorization code is received as a query parameter and is used to
- * retrieve access and refresh tokens from the Reddit API.
- * After obtaining the tokens, the user context is set with the user's username
- * and saves the tokens in the local storage.
- * The user is also redirected back to the thread or home page after authentication.
+ * Component that handles user authentication
+ * Redirects user back to the thread or home page after authentication.
  */
-function Auth() {
+const Auth = () => {
   const navigate = useNavigate();
   const { setUser } = useContext(UserContext);
 
   useEffect(() => {
-    /**
-     * Handles the user login flow.
-     * The authorization code is used to fetch the user tokens from the Reddit
-     * API and the user context is set with the username retrieved from the API.
-     * The tokens are saved in the local storage.
-     *
-     * @param {string} code - The authorization code received as a query parameter.
-     */
-    async function login(code) {
+    const login = async (code: string) => {
       try {
         const tokens = await fetchUserTokens(
           code,
           window.location.origin + "/auth"
         );
         const username = await fetchMe();
-        if (username && tokens.refreshToken) {
+        if (username && tokens?.refreshToken) {
           localStorage.setItem("username", username);
           localStorage.setItem("refresh_token", tokens.refreshToken);
-          setUser(username);
+          if (setUser) setUser(username);
         }
       } catch (error) {
         console.log("error", error);
       }
-    }
+    };
 
     // Handle OAuth redirect
     const search = window.location.search;
     const params = new URLSearchParams(search);
-    const state = JSON.parse(params.get("state"));
+    const state = JSON.parse(params.get("state") ?? "{}");
     const code = params.get("code");
     const threadId = state?.threadId;
 
@@ -60,6 +48,6 @@ function Auth() {
   }, []);
 
   return <Home />;
-}
+};
 
 export default Auth;
