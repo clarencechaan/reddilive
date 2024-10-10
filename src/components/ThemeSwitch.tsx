1c1e4fe08f49b22aa64aadfd47db0af8c038b5fb
Clarence Chan <clarencechaan@icloud.com>
2024-10-10 14:16:18 -0400

    Convert ThemeSwitch to TypeScript

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -6,20 +6,21 @@ import "../styles/ThemeSwitch.css";
 /**
  * Component for a button that toggles between light and dark mode for the app's theme.
  */
-function ThemeSwitch() {
+const ThemeSwitch = () => {
   const { setDarkMode } = useContext(ThemeContext);
 
   /**
    * Toggles the app's theme between light and dark mode, and saves the current
    * state to local storage.
    */
-  function handleThemeSwitchClick() {
-    setDarkMode((prev) => {
-      if (prev) localStorage.setItem("dark_mode", "");
-      else localStorage.setItem("dark_mode", true);
-      return !prev;
-    });
-  }
+  const handleThemeSwitchClick = () => {
+    if (setDarkMode)
+      setDarkMode((prev) => {
+        if (prev) localStorage.setItem("dark_mode", "");
+        else localStorage.setItem("dark_mode", "true");
+        return !prev;
+      });
+  };
 
   return (
     <button className="ThemeSwitch" onClick={handleThemeSwitchClick}>
@@ -27,6 +28,6 @@ function ThemeSwitch() {
       <SunDim size={26} weight="bold" className="sun" />
     </button>
   );
-}
+};
 
 export default ThemeSwitch;
